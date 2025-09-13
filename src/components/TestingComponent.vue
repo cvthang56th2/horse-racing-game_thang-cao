a25261735fad4c9521ff9572c8b853eff2fd1f10
Thang Cao <cvthang56th2@gmail.com>
2025-09-13 08:18:43 +0700

    install tailwindcss

---
+++ src/components/TestingComponent.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="w-full h-full flex flex-col items-center justify-center">
+    <h1>Testing Component</h1>
+    <p>This is a test component.</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { defineComponent } from 'vue';
+defineComponent({
+  name: 'TestingComponent',
+});
+</script>
+
+<style scoped>
+
+</style>
